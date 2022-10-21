6ffec78c2b97e7bc6cc6e560c8fb2021e4000d15
raeeceip <chiboguchisomu@gmail.com>
2022-10-21 03:29:36 +0100

    adding changes

--- src/Components/Pages/Projects/Projects.jsx
+++ src/Components/Pages/Projects/Projects.jsx
@@ -3,8 +3,19 @@ import React from "react";
 
 const Projects = () => {
 	return (
-		<div>
-			<h1>Posts</h1>
+		<div className="flex flex-col justify-center items-center h-screen">
+			<div className="card flex flex-col justify-center items-center">
+				<p>Btsfy</p>
+				<h3> A web app that splits csv and json files</h3>
+				<p>React, Tailwind, Django</p>
+				<a href="https://btsfy.netlify.app/">View</a>
+			</div>
+			<div className="card flex flex-col justify-center items-center">
+				<p>Chisom</p>
+				<h3> A web app that splits csv and json files</h3>
+				<p>React, Tailwind, Django</p>
+				<a href="https://btsfy.netlify.app/">View</a>
+			</div>
 		</div>
 	);
 };
--- src/Components/Pages/body/body.jsx
+++ src/Components/Pages/body/body.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { useState } from "react";
+import "./body.css";
 
 const Body = () => {
 	const [showPage, setShowPage] = useState(false);
 	return (
 		<div className="relative sm:flex h-[80vh] lg:grid md:grid place-items-center grid-flow-col w-screen  lg:top-2 top-10 overflow-y-hidden">
-			<div className=" text-left  justify-center mx-5 mt-20  ">
-				<h1 className="lg:text-5xl sm:text-5xl m-2 text-white font-bold">
-					Hi there, I'm Chibogu Chisom
-				</h1>
+			<div className=" text-left  justify-center mx-5 mt-10  ">
+				<div className="typewriter">
+					<h1 className="typewriter lg:text-5xl sm:text-5xl m-2 text-white font-bold">
+						Chiso,
+					</h1>
+					<h2 className="typewriter lg:text-5xl sm:text-5xl m-2 text-white font-bold">
+						Software Developer
+					</h2>
+				</div>
+
 				<p className="text-white m-2 lg:text-xl">
 					I'm a fullstack web developer with hands-on experience on building,
-					deploying and testing various web applications and software. With
-					knowledge of React.js, Django, Tailwind and Bootstrap
+					deploying and testing various web applications and software.
+				</p>
+				<p className="text-white m-2 lg:text-xl">
+					{" "}
+					With knowledge of React.js, Django, Tailwind and Bootstrap
 				</p>
 				<p className="text-white m-2 lg:text-xl sm:text-xl">
 					Based in Lagos, Nigeria
@@ -20,19 +30,14 @@ const Body = () => {
 				<p className="text-white  lg:text-xl">
 					Available for Contract and Full-Time Jobs
 				</p>
-				<div>
+				<div className="flex">
 					<button className="bg-white text-black p-2 rounded-md m-5">
 						<a href="mailto:chiboguchisomu@gmail.com">Lets Talk</a>
 					</button>
+					<button className="bg-white text-black p-2 rounded-md m-5">
+						<a href="/projects">Check out some of my projects</a>
+					</button>
 				</div>
-			</div>
-
-			<div className=" invisible grid-cols-1 lg:visible md:visible my-10 mx-10  w-100  ">
-				<img
-					className="rounded-full"
-					src="https://github.com/raeeceip.png"
-					alt="Chiso."
-				/>
 			</div>
 		</div>
 	);
