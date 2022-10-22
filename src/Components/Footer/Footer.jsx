55365c29416f5b0bb17f001bbc5745820e55084b
raeeceip <chiboguchisomu@gmail.com>
2022-10-22 02:06:43 +0100

    fa-icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
 	return (
-		<div className="absolute flex w-full top-[95vh]  bottom-0  p-6 text-white justify-between">
+		<div className="absolute flex w-full top-[90vh]  bottom-0  p-6 text-white justify-between">
 			<h1 className="ml-0">Chiso</h1>
 			<ul className="flex justify-evenly mr-0">
 				<li className="mr-0">
 					<a href="#" className="header__nav-link">
 						Contact
+					</a>
+				</li>
+				<li className="mr-0">
+					<a href="#" className="header__nav-link">
+						<FontAwesomeIcon icon={faGithub} />
+					</a>
+				</li>
+				<li className="mr-0">
+					<a href="#" className="header__nav-link">
+						<FontAwesomeIcon icon={faLinkedin} />
 					</a>
 				</li>
 			</ul>
--- src/Components/Pages/body/body.jsx
+++ src/Components/Pages/body/body.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import "./body.css";
+import { Link } from "react-router-dom";
 
 const Body = () => {
 	const [showPage, setShowPage] = useState(false);
@@ -8,24 +9,24 @@ const Body = () => {
 		<div className="relative sm:flex h-[70vh] lg:grid md:grid place-items-center grid-flow-col w-screen  lg:top-2 top-10 overflow-y-hidden">
 			<div className=" text-left  justify-center mx-5 mt-10  ">
 				<div className="typewriter">
-					<h1 className="typewriter text-4xl m-2 text-white font-bold">
+					<h1 className="typewriter text-4xl m-3 text-white font-bold">
 						Hello!, I'm Chiso
 					</h1>
 				</div>
 
-				<p className="text-white m-2 lg:text-xl">
+				<p className="text-white m-3 lg:text-xl">
 					I'm a fullstack web developer with hands-on experience on building,
 					deploying and testing various web applications and software.
 				</p>
-				<p className="text-white m-2 lg:text-xl">
+				<p className="text-white m-3 lg:text-xl sm:text-3xl">
 					{" "}
 					Focused on building interactive experiences for exceptional people and
 					brands on the web
 				</p>
-				<p className="text-white m-2 lg:text-xl sm:text-xl">
+				<p className="text-white m-3 lg:text-xl sm:text-xl">
 					Based in Lagos, Nigeria
 				</p>
-				<p className="text-white  lg:text-xl">
+				<p className="text-white m-3 lg:text-xl sm:text-xl">
 					Available for Contract and Full-Time Jobs
 				</p>
 				<div className="flex">
@@ -33,7 +34,7 @@ const Body = () => {
 						<a href="mailto:chiboguchisomu@gmail.com">Lets Talk</a>
 					</button>
 					<button className="bg-white text-black p-2 rounded-md m-5">
-						<a href="/projects">Check out some of my projects</a>
+						<Link to="/projects">Check out some of my projects</Link>
 					</button>
 				</div>
 			</div>
