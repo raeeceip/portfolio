9b433d7af00d59c18a78f81f0c5a1b965fb06e8f
raeeceip <chiboguchisomu@gmail.com>
2022-10-23 20:46:48 +0100

    another fix

--- src/Components/Pages/body/about.jsx
+++ src/Components/Pages/body/about.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
 	return (
 		<div>
-			<div className="relative h-[85vh] flex justify-center mx-5 ">
+			<div className="relative h-[85vh] flex text-center space-between justify-center mx-10 ">
 				<div className="sm:w-1/2">
 					<h1 className="text-4xl">About</h1>
 					<p className="text-2xl">
@@ -12,6 +12,32 @@ const About = () => {
 						a fast learner. I love to learn new things and I am always open to
 						learning new technologies.
 					</p>
+					<h2 className="border-b text-2xl text-center"> Skills </h2>
+					<p className="text-2xl">
+						I'm skilled with using the following to create amazing projects
+					</p>
+					<ul className="flex justify-evenly">
+						<li>HTML</li>
+						<li>CSS</li>
+						<li>JavaScript</li>
+						<li>React</li>
+						<li>Node</li>
+						<li>Django</li>
+						<li>Python</li>
+						<li>SQL</li>
+						<li>Git</li>
+					</ul>
+					<p className="text-2xl">
+						{" "}
+						I'm also familiar with the following tools
+					</p>
+					<ul className="flex justify-evenly">
+						<li>Vercel</li>
+						<li>Express</li>
+						<li>Heroku</li>
+						<li>Netlify</li>
+						<li>Git</li>
+					</ul>
 				</div>
 			</div>
 		</div>
